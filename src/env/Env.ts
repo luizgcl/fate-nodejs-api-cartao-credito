4136d1566f3ba1ad158c2ad8d0dd5a30b78992a2
luizgcl <luiizgcl@gmail.com>
2024-10-17 11:59:33 -0300

    chore: add DEBUG in environment variables

--- src/env/Env.ts
+++ src/env/Env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
+  DEBUG: z.coerce.boolean().default(false),
 })
 
 export const env = envSchema.parse(process.env)
